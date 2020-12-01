93a0ff2a566ae629f86bac06159a3ee9fc128eb0
hienha <hathihien85@gmail.com>
2020-12-01 08:52:24 +0100

    add tesseractOCR.py
    create json input file for CUTIE using Tesseract OCR

---
+++ README.md
@@ -0,0 +1,43 @@
+# CUTIE
+
+TensorFlow implementation of the paper "CUTIE: Learning to Understand Documents with Convolutional Universal Text Information Extractor."
+Xiaohui Zhao [ArXiv 2019](https://arxiv.org/abs/1903.12363v4)
+
+## Results
+
+Result evaluated on 4,484 receipt documents, including taxi receipts, meals entertainment receipts, and hotel receipts, with 9 different key information classes. (AP / softAP)
+|Method     | #Params   |  Taxi         |  Hotel        |
+| ----------|:---------:| :-----:       | :-----:       |
+| CloudScan | -         |  82.0 / -     |  60.0 / -     |
+| BERT      | 110M      |  88.1 / -     |  71.7 / -     |
+| CUTIE     |**14M**    |**94.0 / 97.3**|**74.6 / 87.0**|
+
+![Taxi](https://github.com/vsymbol/CUTIE/raw/master/others/example_1.jpg)
+
+![Hotel](https://github.com/vsymbol/CUTIE/raw/master/others/example_2.jpg)
+
+
+## Installation & Usage
+
+```
+pip install -r requirements.txt
+```
+
+1. Generate your own dictionary with main_build_dict.py / main_data_tokenizer.py
+2. Train your model with main_train_json.py
+
+CUTIE achieves best performance with rows/cols well configured. For more insights, refer to statistics in the file (others/TrainingStatistic.xlsx).
+
+![Chart](https://github.com/vsymbol/CUTIE/raw/master/others/chart.jpg)
+
+
+## TLDR
+
+For information about the input example, refer to [issue discussion](https://github.com/vsymbol/CUTIE/issues/7).
+- Apply any OCR tool that help you detecting and recognizing words in the scanned document image.
+- Label image OCR results with key information class as the .json file in the invoice_data folder. (thanks to @4kssoft)
+
+To create json input file using Tesseract OCR:
+    python3 tesseractOCR.py -i path-to-image -l language > ouput.json
+e.g:
+python3 tesseractOCR.py -i ./invoice_data/Faktura1.pdf_0.jpg -l pol > Faktura1.json
---
+++ tesseractOCR.py
@@ -0,0 +1,84 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+# author: Ha Thi Hien
+# description: Run tesseract OCR, output json file for CUTIE
+# date: 1.12.2020
+
+from PIL import Image
+import pytesseract
+import sys
+# from lxml import etree
+from os.path import split, splitext
+import json
+
+def readTSV(data):
+    rows = data.split('\n')
+    nlen = len(rows)
+    if nlen == 0:
+        return
+    doc = []
+    # get columns
+    c = dict() # column
+    #level	page_num	block_num	par_num	line_num	word_num	left	top	width	height	conf	text
+    header = rows[0].split('\t')    
+
+    npage = None # page#
+    nbl = None
+    nline = None
+    for i in range(1, nlen):
+        r = rows[i].split('\t')        
+        word = dict()
+        word['id'] = i        
+        word['bbox'] = tuple([int(r[6]), int(r[7]), int(r[6])+ int(r[8]), int(r[7])+ int(r[9])])
+        if len(r) < 12:
+            #sys.stderr.write('not full fields')            
+            word['text'] = ""
+        else:
+            word['text'] = r[11]
+        if word['text']:            
+            doc.append(word)
+    return doc
+
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser(description='Doing TesseractOCR')
+    
+    parser.add_argument('-i', '--input', type=str,
+                        required=False, default=sys.stdin,
+                        help='Input image file')
+    parser.add_argument('-l', '--language', type=str,
+                        required=False, default='eng',
+                        help='language')
+    parser.add_argument('-o', '--output', type=argparse.FileType('w'),
+                        required=False, default=sys.stdout,
+                        help='Output')
+    
+    args = parser.parse_args()
+    
+    with Image.open(args.input) as img:
+        # width, height = img.size
+        doc = pytesseract.image_to_data(img, lang=args.language, config='--psm 11')#custom_config
+        
+        doc = readTSV(doc)
+        
+        new_doc = {}
+        new_doc.update({'text_boxes':doc})
+        
+        classes = ['DATE_CREATION', 'DATE_EXECUTION', 'NAME_SELLER', 'ADDR_SELLER']
+        fields = []
+        for cl in classes:
+            new_field = {"field_name": cl, "value_id": [], "value_text":[], "key_id":[], "key_text":[]}
+            fields.append(new_field)
+        new_doc.update({'fileds': fields})
+
+        new_doc.update({"global_attributes":{"file_id": args.input}})        
+        
+        json_obj = json.dumps(new_doc, indent = 4, ensure_ascii=False)
+        
+        args.output.write(json_obj)
+
+
+if __name__ == "__main__":
+    main()
